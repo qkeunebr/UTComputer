b1c2ab7b8a28fa5bdfb63a9f16fa46131789246b
Rtrouve <rtrouve@hotmail.fr>
2016-05-28 17:43:25 +0200

    Update fenetre.h

--- fenetre.h
+++ fenetre.h
@@ -11,31 +11,71 @@
 #include <QCheckBox>
 #include <QWidget>
 #include <iostream>
+#include <windows.h>
+#include <QTableWidget>
+#include <QHeaderView>
+#include "controller.h"
+#include "litteral.h"
+
 
 class Fenetre : public QMainWindow {
+Q_OBJECT
+    Controller* cont;
+    QLineEdit* entree;
     //Un attribut qui contient l'application/l'objet auquelle on fait appel
 
     //Attribut affichage clavier
-//    QWidget calcButton;
+    QWidget* calcButton;
     //Attribut bip sonore
     bool son;
-    /*
+
+    QLineEdit* message;
+    QTableWidget* vuePile;
+    QLineEdit* commande;
+    QVBoxLayout* couche;
+    Controller* controleur;
 public slots :
     void checkClavierStateChanged(int state){
-        if(state)   calcButton.show();
-        else    calcButton.hide();
+        if(state)   calcButton->show();
+        else    calcButton->hide();
     }
 
     void checkSonStateChanged(int state){
         son = state;
     }
-    */
 
+    void pushB1();
+    void pushB2();
+    void pushB3();
+    void pushB4();
+    void pushB5();
+    void pushB6();
+    void pushB7();
+    void pushB8();
+    void pushB9();
+    void pushB0();
+    void pushBPlus();
+    void pushBMoins();
+    void pushBFois();
+    void pushBDiv();
+    void pushBNeg();
+    void pushBNum();
+    void pushBDen();
+    void pushBCompl();
+    void pushBRe();
+    void pushBIm();
+    void pushBEg();
+    void pushBNEg();
+    void pushBEgSup();
+    void pushBEgInf();
+    void pushBSup();
+    void pushBInf();
+    void pushBOr();
+    void pushBAnd();
+    void pushBNot();
 public :
 
     Fenetre();
-
-
 };
 
 
